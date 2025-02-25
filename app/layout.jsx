edd0ccf60fc9b2173140c1e1be19e82115335090
Kevin Yang-Li <yanglikevin@gmail.com>
2025-02-25 00:56:41 -0500

    wrote the title of the page and the logo in the navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,27 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: 'Aulora',
-    description: 'Aulora is a platform for free copyright AI content',
-}
- 
+  title: "Aulora",
+  description: "Aulora is a platform for free copyright AI content",
+};
+
 const RootLayout = ({ children }) => {
   return (
-    <html lang = "en">
-        <body>
-            <div className = "main">
-                <div className = "gradient"></div>
-            </div>
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
 
-            <main className = "app">
-                {children}
-            </main>
-        </body>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
+import Feed from "@components/Feed"
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className = "w-full flex-center flex-col">
+      <h1 className = "head_text text-center">
+        Discover and Share
+        <br className = "max-md:hidden"/>
+        <span className = "orange_gradient text-center"> Copyright free AI Content</span>
+      </h1>
+
+      <p className = "desc text-center">
+        Aulora provides Copyright-free AI content. Share your content with the world.
+      </p>
+
+      <Feed />
+    </section>
   )
 }
 
