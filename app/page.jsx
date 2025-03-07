1878ff1cd2c5c0ef6c7404111740238718a77605
Kevin Yang-Li <yanglikevin@gmail.com>
2025-03-06 22:35:25 -0500

    created the navbar for signed in users

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,24 @@
-import Feed from "@components/Feed"
+import Feed from "@components/Feed";
 const Home = () => {
   return (
-    <section className = "w-full flex-center flex-col">
-      <h1 className = "head_text text-center">
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
         Discover and Share
-        <br className = "max-md:hidden"/>
-        <span className = "orange_gradient text-center"> Copyright free AI Content</span>
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          {" "}
+          Copyright free AI Content
+        </span>
       </h1>
 
-      <p className = "desc text-center">
-        Aulora provides Copyright-free AI content. Share your content with the world.
+      <p className="desc text-center">
+        Aulora provides Copyright-free AI content. Share your content with the
+        world.
       </p>
 
       <Feed />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- components/nav.jsx
+++ components/nav.jsx
@@ -6,6 +6,9 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -18,6 +21,32 @@ const Nav = () => {
         />
         <p className="logo_text">Aulora</p>
       </Link>
+
+      {/* desk Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" className="outline-btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile" className="">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                classsName="rounded-full"
+                alt="profile picture"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
